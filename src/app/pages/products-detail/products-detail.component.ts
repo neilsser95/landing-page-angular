30f73659b623594f85964a9a4f0bde97a21ff6ed
neilsser95 <neilsalti95@gmail.com>
2024-10-11 12:47:22 -0400

    hice una landing page

---
+++ src/app/pages/products-detail/products-detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../models/product.models';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-products-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products-detail.component.html',
+  styleUrl: './products-detail.component.css'
+})
+export class ProductsDetailComponent implements OnInit{
+
+  loading: boolean = true;
+  public product?: IProduct;
+
+  private _route = inject(ActivatedRoute);
+  private _apiService = inject(ApiService);
+
+  ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      this._apiService.getProduct(params['id']).subscribe((data: IProduct) =>{
+        this.product = data;
+        this.loading = false;
+      })
+    })
+  }
+}
